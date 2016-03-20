343ddf2b133f077851d7a5b099d1d8733fc34ba6
Basma <Basma>
2016-03-19 20:50:44 -0400

    Commit method Continuelevel

--- src/Board.java
+++ src/Board.java
@@ -8,7 +8,9 @@ public class Board {
     private int viewdx;
     private int viewdy;
     private int nbofghosts = 6;
-
+    private final int blocksize = 24;
+    private int currentspeed = 3;
+    private boolean dying = false;
     private Ghost ghosts[];
     public int getDirdx() {
         return dirdx;
@@ -30,12 +32,32 @@ public class Board {
 
     private void continueLevel()
     {
-        short i;
+        int i;
         int dx =1;
         int random;
 
-        for(i =0; i< nbofghosts; i++) {
+        for(i =0; i< nbofghosts; i++)
+        {
+            ghosts[i] = new Ghost();
+            ghosts[i].setX(4*blocksize);
+            ghosts[i].setY(4*blocksize);
+            ghosts[i].setDy(0);
+            ghosts[i].setDx(dx);
+            dx =-dx;
+            random = (int) (Math.random() * (currentspeed +1));
+            if(random > currentspeed)
+                random = currentspeed;
 
+            Pacman p = new Pacman();
+            p.setX(7*blocksize);
+            p.setY(11*blocksize);
+            p.setDx(0);
+            p.setDy(0);
+            dirdx=0;
+            dirdy=0;
+            viewdx=-1;
+            viewdy=0;
+            dying = false;
         }
     }
 
